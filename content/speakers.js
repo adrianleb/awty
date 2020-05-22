3fa2767d221d0614f6a6f251b32b116b51c4d381
Lili <li.saengsanthitam@gmail.com>
2020-05-22 17:05:57 +0200

    speakers details and picture

--- content/speakers.js
+++ content/speakers.js
@@ -1,171 +1,274 @@
 const speakers = [
   {
-    title: 'Proactive scalable local area network',
+    title: 'Everything in place, nothing in order',
     time: '2020-05-31T11:00',
     speakers: [
       {
-        name: 'Dexter Nairns',
-        jobTitle: 'Payment Adjustment Coordinator',
+        name: 'Davide Bernadis',
+        jobTitle: 'Creative Director',
+        photo:
+          'speaker1/davide.png',
+      },
+    ],
+  },
+  {
+    title: 'Digital Theme Park Platforms: What\'s the future of clothing?',
+    time: '2020-05-31T11:00',
+    speakers: [
+      {
+        name: 'Benny Giang',
+        jobTitle: 'Technologist, Dapper Lab',
+        photo:
+          'speaker1/benny.png',
+      },
+    ],
+  },
+  {
+    title: '123 Draw! A different approach to creativity',
+    time: '2020-05-31T11:00',
+    speakers: [
+      {
+        name: 'Esté Macleod',
+        jobTitle: 'Artist & Educator',
+        photo:
+          'speaker1/este.png',
+      },
+    ],
+  },
+  {
+    title: 'ARE-WE-DANCING-YET?',
+    time: '2020-05-31T11:00',
+    speakers: [
+      {
+        name: 'Klasien van de Zandschulp',
+        jobTitle: 'Artist',
+        photo:
+          'speaker1/klasien.png',
+      },
+    ],
+  },
+  {
+    title: 'Spread Art: test your creativity and transmit it',
+    time: '2020-05-31T11:00',
+    speakers: [
+      {
+        name: 'Inma Moreno',
+        jobTitle: 'Artist',
+        photo:
+          'speaker1/inma.png',
+      },
+    ],
+  },
+  {
+    title: 'How you can influence the fashion industry in becoming more sustainable?',
+    time: '2020-05-31T11:00',
+    speakers: [
+      {
+        name: 'Leonor Von Salisch',
+        jobTitle: 'Fashion Activist, XO Berlin',
+        photo:
+          'speaker1/leonor.png',
+      },
+    ],
+  },
+  {
+    title: 'The New Normal - Are We All Right or Orwell? An Installation to commemorate the voluntarily surrender    ',
+    time: '2020-05-31T11:00',
+    speakers: [
+      {
+        name: 'Martin Lukas Ostachowski',
+        jobTitle: 'Artist',
+        photo:
+          'speaker1/martin.png',
+      },
+    ],
+  },
+  {
+    title: 'Let\'s get existential! Anthropocene Heritage before and after COVID19 multi-transitional crisis.',
+    time: '2020-05-31T11:00',
+    speakers: [
+      {
+        name: 'Tainá Guedes',
+        jobTitle: 'Artist, Creator of Food Art Week',
         photo:
           'https://robohash.org/inciduntaliquidtempore.bmp?size=200x200&set=set1',
       },
     ],
   },
   {
-    title: 'Proactive scalable local area network',
+    title: 'Feminism in Surrealist Paintings',
     time: '2020-05-31T11:00',
     speakers: [
       {
-        name: 'Dexter Nairns',
-        jobTitle: 'Payment Adjustment Coordinator',
+        name: 'Puntita Meeboonsabai',
+        jobTitle: 'Artist, Painter',
+        photo:
+          'speaker1/puntita.png',
+      },
+    ],
+  },
+  {
+    title: 'How does music support you? The Importance & Value of Songs of Substance',
+    time: '2020-05-31T11:00',
+    speakers: [
+      {
+        name: 'Jasmin Morgner',
+        jobTitle: 'Musician',
+        photo:
+          'speaker1/jasmin.png',
+      },
+    ],
+  },
+  {
+    title: 'Inclusive Design for Situational Limitations',
+    time: '2020-05-31T11:00',
+    speakers: [
+      {
+        name: 'Evan Minto',
+        jobTitle: 'User-Experience Designer',
+        photo:
+          'speaker1/evan.png',
+      },
+    ],
+  },
+  {
+    title: 'Welcome to the Precipice',
+    time: '2020-05-31T11:00',
+    speakers: [
+      {
+        name: 'Duncan',
+        jobTitle: 'Reality Technician',
         photo:
           'https://robohash.org/inciduntaliquidtempore.bmp?size=200x200&set=set1',
       },
     ],
   },
   {
-    title: 'Proactive scalable local area network',
+    title: 'What does a Decentralized Art World Feel Like?',
     time: '2020-05-31T11:00',
     speakers: [
       {
-        name: 'Dexter Nairns',
-        jobTitle: 'Payment Adjustment Coordinator',
-        photo:
-          'https://robohash.org/inciduntaliquidtempore.bmp?size=200x200&set=set1',
-      },
-    ],
-  },
-  {
-    title: 'Proactive scalable local area network',
-    time: '2020-05-31T11:00',
-    speakers: [
-      {
-        name: 'Dexter Nairns',
-        jobTitle: 'Payment Adjustment Coordinator',
-        photo:
-          'https://robohash.org/inciduntaliquidtempore.bmp?size=200x200&set=set1',
-      },
-    ],
-  },
-  {
-    title: 'Proactive scalable local area network',
-    time: '2020-05-31T11:00',
-    speakers: [
-      {
-        name: 'Dexter Nairns',
-        jobTitle: 'Payment Adjustment Coordinator',
-        photo:
-          'https://robohash.org/inciduntaliquidtempore.bmp?size=200x200&set=set1',
-      },
-    ],
-  },
-  {
-    title: 'Proactive scalable local area network',
-    time: '2020-05-31T11:00',
-    speakers: [
-      {
-        name: 'Dexter Nairns',
-        jobTitle: 'Payment Adjustment Coordinator',
-        photo:
-          'https://robohash.org/inciduntaliquidtempore.bmp?size=200x200&set=set1',
-      },
-    ],
-  },
-  {
-    title: 'Proactive scalable local area network',
-    time: '2020-05-31T11:00',
-    speakers: [
-      {
-        name: 'Dexter Nairns',
-        jobTitle: 'Payment Adjustment Coordinator',
-        photo:
-          'https://robohash.org/inciduntaliquidtempore.bmp?size=200x200&set=set1',
-      },
-    ],
-  },
-  {
-    title: 'Proactive scalable local area network',
-    time: '2020-05-31T11:00',
-    speakers: [
-      {
-        name: 'Dexter Nairns',
-        jobTitle: 'Payment Adjustment Coordinator',
-        photo:
-          'https://robohash.org/inciduntaliquidtempore.bmp?size=200x200&set=set1',
-      },
-    ],
-  },
-  {
-    title: 'Proactive scalable local area network',
-    time: '2020-05-31T11:00',
-    speakers: [
-      {
-        name: 'Dexter Nairns',
-        jobTitle: 'Payment Adjustment Coordinator',
-        photo:
-          'https://robohash.org/inciduntaliquidtempore.bmp?size=200x200&set=set1',
-      },
-    ],
-  },
-  {
-    title: 'Proactive scalable local area network',
-    time: '2020-05-31T11:00',
-    speakers: [
-      {
-        name: 'Dexter Nairns',
-        jobTitle: 'Payment Adjustment Coordinator',
-        photo:
-          'https://robohash.org/inciduntaliquidtempore.bmp?size=200x200&set=set1',
-      },
-    ],
-  },
-  {
-    title: 'Proactive scalable local area network',
-    time: '2020-05-31T11:00',
-    speakers: [
-      {
-        name: 'Dexter Nairns',
-        jobTitle: 'Payment Adjustment Coordinator',
-        photo:
-          'https://robohash.org/inciduntaliquidtempore.bmp?size=200x200&set=set1',
-      },
-    ],
-  },
-  {
-    title: 'Proactive scalable local area network',
-    time: '2020-05-31T11:00',
-    speakers: [
-      {
-        name: 'Dexter Nairns',
-        jobTitle: 'Payment Adjustment Coordinator',
-        photo:
-          'https://robohash.org/inciduntaliquidtempore.bmp?size=200x200&set=set1',
-      },
-    ],
-  },
-  {
-    title: 'Implemented cohesive instruction set',
-    time: '2020-05-31T11:00',
-    speakers: [
-      {
-        name: 'Zaria Eglinton',
-        jobTitle: 'Database Administrator II',
+        name: 'Ilan Katin',
+        jobTitle: 'Artist',
         photo:
           'https://robohash.org/praesentiumcommodialiquam.png?size=200x200&set=set1',
       },
       {
-        name: 'Zaria Eglinton',
-        jobTitle: 'Database Administrator II',
+        name: 'Conlan Rios',
+        jobTitle: 'Technologist, Async.art',
         photo:
-          'https://robohash.org/praesentiumcommodialiquam.png?size=200x200&set=set1',
+          'speaker1/conlan.png',
       },
       {
-        name: 'Zaria Eglinton',
-        jobTitle: 'Database Administrator II',
+        name: 'VaX',
+        jobTitle: 'Crypto Artist',
         photo:
-          'https://robohash.org/praesentiumcommodialiquam.png?size=200x200&set=set1',
+          'speaker1/vax.png',
+      },
+      {
+        name: 'Sparrow',
+        jobTitle: 'Artist',
+        photo:
+          'speaker1/sparrow.png',
       },
     ],
   },
+  {
+    title: 'Ghosting your own project? 4 strategies to deal with your own downsides',
+    time: '2020-05-31T11:00',
+    speakers: [
+      {
+        name: 'Greta Ferreira ',
+        jobTitle: 'Creator, House of Girls Collective',
+        photo:
+          'speaker1/greta.png',
+      },
+    ],
+  },
+  {
+    title: 'Creative sustainability',
+    time: '2020-05-31T11:00',
+    speakers: [
+      {
+        name: 'Eilidh McLaughlin',
+        jobTitle: 'Sustainability Consultant',
+        photo:
+          'https://robohash.org/inciduntaliquidtempore.bmp?size=200x200&set=set1',
+      },
+    ],
+  },
+  {
+    title: 'Fashion sustainability through hyper-personalization',
+    time: '2020-05-31T11:00',
+    speakers: [
+      {
+        name: 'Tanisara Borwonnauwarux',
+        jobTitle: 'Fashion Designer',
+        photo:
+          'speaker1/tanisara.png',
+      },
+    ],
+  },
+  {
+    title: 'The sound of existentialism is a shutter click',
+    time: '2020-05-31T11:00',
+    speakers: [
+      {
+        name: 'Graham Meyer',
+        jobTitle: 'Photographer',
+        photo:
+          'speaker1/graham.png',
+      },
+    ],
+  },
+  {
+    title: 'Virtually Rare Experiences',
+    time: '2020-05-31T11:00',
+    speakers: [
+      {
+        name: 'James Waugh',
+        jobTitle: 'Technologist, RareAF',
+        photo:
+          'speaker1/james.png',
+      },
+    ],
+  },
+  {
+    title: 'How learning life drawing can transform creative work?',
+    time: '2020-05-31T11:00',
+    speakers: [
+      {
+        name: 'Leo Rain',
+        jobTitle: 'Art Educator',
+        photo:
+          'speaker1/leorain.png',
+      },
+    ],
+  },
+  {
+    title: 'Evolution of Music',
+    time: '2020-05-31T11:00',
+    speakers: [
+      {
+        name: 'RAC - André Allen Anjos',
+        jobTitle: 'Artist',
+        photo:
+          'speaker1/racAndreAllenAnjos.png',
+      },
+      {
+        name: 'Jack Spallone',
+        jobTitle: 'Technologist, Async.art',
+        photo:
+          'speaker1/jackspallone.png',
+      },
+      {
+        name: 'Simona Pop',
+        jobTitle: 'Technologist, OST',
+        photo:
+          'speaker1/simona.png',
+      },
+    ],
+  },
+  
 ];
 export default speakers;
--- pages/_app.js
+++ pages/_app.js
@@ -40,31 +40,31 @@ export default class App extends NextApp {
           />
           <meta
             name="twitter:image:src"
-            content="../public/twitter/tt-card-summary.png"
+            content="twitter/tt-card-summary.png"
           />
 
           {/* Favicon */}
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="../public/favicon/apple-touch-icon.png"
+            href="favicon/apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="../public/favicon/favicon-32x32.png"
+            href="favicon/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="../public/favicon/favicon-16x16.png"
+            href="favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="../public/favicon/site.webmanifest" />
           <link
             rel="mask-icon"
-            href="../public/favicon/safari-pinned-tab.svg"
+            href="favicon/safari-pinned-tab.svg"
             color="#5bbad5"
           />
           <meta name="msapplication-TileColor" content="#da532c" />
